b14b0e4b26e9138ef7ae995a246ad2da38ac5c3f
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-02-08 14:59:58 +0200

    Seems to work for simple correct cases

--- 1/scanner.py
+++ 1/scanner.py
@@ -3,9 +3,13 @@ Leo Kivikunnas 525925
 Jaakko Koskela 526050
 """
 
+
+import sys
+
+
 SYMBOL = ["=", ";", ":", ",", "[", "]", "(", ")", "{", "}",
           "+", "-", "*", "=", "<"]
-KEYWORD = ["if", "else", "void", "int", "while", "break", "continue", "switch", "default", "case", "return"] 
+KEYWORD = ["if", "else", "void", "int", "while", "break", "continue", "switch", "default", "case", "return"]
 
 def strip_from_start(string, substring):
     return string.replace(substring, "", 1)
@@ -52,6 +56,7 @@ def match_symbol(string, substring):
 def match_comment(string, substring, lineno):
     if substring == "/":
         if string[1] == "*":
+            substring += string[1]
             prev = ""
             for char in string[2:]:
                 substring += char
@@ -59,7 +64,7 @@ def match_comment(string, substring, lineno):
                     lineno += 1
                 if prev == "*" and char == "/":
                     break
-            prev = char
+                prev = char
 
         if string[1] == "/":
             for char in string[2:]:
@@ -73,6 +78,7 @@ def match_comment(string, substring, lineno):
 
     return False, None, None, None
 
+
 def match_keyword(string, substring):
     if substring in [s[0] for s in KEYWORD]:
         for char in string[1:]:
@@ -80,7 +86,7 @@ def match_keyword(string, substring):
             if substring in KEYWORD:
                 string = strip_from_start(string, substring)
                 return True, string, substring
-            if len(substring) <= len(max(KEYWORD, key=len)):
+            if len(substring) > len(max(KEYWORD, key=len)):
                 break
 
     return False, None, None
@@ -92,6 +98,7 @@ def match_id(string, substring):
             if char.isdigit() or char.isalpha():
                 substring += char
             else:
+                string = strip_from_start(string, substring)
                 break
 
         return True, string, substring
@@ -114,7 +121,7 @@ def match(string, lineno):
     matched, new_string, matched_string = match_num(string, substring)
     if matched:
         token = "num"
-        return new_string, token, matched_string, new_lineno
+        return new_string, token, matched_string, lineno
 
     matched, new_string, matched_string = match_symbol(string, substring)
     if matched:
@@ -140,6 +147,7 @@ def match(string, lineno):
         token = "id"
         return new_string, token, matched_string, lineno
 
+    sys.exit()
 
 def write_output(token, substring, lineno, out):
     if not lineno in out:
@@ -165,7 +173,7 @@ def get_next_token(data, symbol_table, lineno, out):
 
 
 def main():
-    with open("test.txt") as f:
+    with open("no_errors.txt") as f:
         data = f.read()
         data = data.rstrip("\n")
         f.close
@@ -178,8 +186,10 @@ def main():
         data, lineno = get_next_token(data, symbol_table, lineno, out)
 
     print("\nTokens:")
-    print(out)
-
+    for key in out:
+        out[key] = [t for t in out[key] if t[0] not in ["whitespace", "comment"]]
+        if len(out[key]) > 0:
+            print(out[key])
 
     symbol_table = list(dict.fromkeys(symbol_table))
     print("\nSymbol table:")
