db800e50fc79148f34ad81b1c3f89001e29f4f0f
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-02-08 14:09:35 +0200

    Refactor regex as something DFA like

--- 1/scanner.py
+++ 1/scanner.py
@@ -3,67 +3,161 @@ Leo Kivikunnas 525925
 Jaakko Koskela 526050
 """
 
-import re
+SYMBOL = ["=", ";", ":", ",", "[", "]", "(", ")", "{", "}",
+          "+", "-", "*", "=", "<"]
+KEYWORD = ["if", "else", "void", "int", "while", "break", "continue", "switch", "default", "case", "return"] 
+
+def strip_from_start(string, substring):
+    return string.replace(substring, "", 1)
 
 
-KEYWORD = r"if|else|void|int|while|break|continue|switch|default|case|return"
-ID = r"[A-Za-z][A-Za-z0-9]*"
-NUM = r"[0-9]+"
-SYMBOL = r"==|[;:,\[\]\(\)\{\}\+\-\*\=\<]"
-COMMENT = r"\/\*.*\*\/|//.*\n"
-WHITESPACE = r"[\ \n\r\t\v\f]"
+def match_whitespace(string, substring, lineno):
+    if substring.isspace():
+        if (substring == "\n"):
+            lineno += 1
+
+        string = strip_from_start(string, substring)
+        return True, string, substring, lineno
+
+    return False, None, None, None
 
 
-def match(string):
-    m = re.match(KEYWORD, string)
-    if m:
-        return "keyword", m.group(0)
+def match_num(string, substring):
+    if substring.isdigit():
+        for char in string[1:]:
+            if char.isdigit():
+                substring += char
+            else:
+                break
 
-    m = re.match(ID, string)
-    if m:
-        return "id", m.group(0)
+        string = strip_from_start(string, substring)
+        return True, string, substring
 
-    m = re.match(NUM, string)
-    if m:
-        return "num", m.group(0)
-
-    m = re.match(SYMBOL, string)
-    if m:
-        return "symbol", m.group(0)
-
-    m = re.match(COMMENT, string)
-    if m:
-        return "comment", m.group(0)
-
-    m = re.match(WHITESPACE, string)
-    if m:
-        return "whitespace", m.group(0)
-
-    else:
-        return None
+    return False, None, None
 
 
-def write_output(token, sub_string, lineno, out):
+def match_symbol(string, substring):
+    if substring in SYMBOL:
+        if substring == "=" and string[1] == "=":
+            substring += string[1]
+            string = strip_from_start(string, substring)
+        else:
+            string = strip_from_start(string, substring)
+
+        return True, string, substring
+
+    return False, None, None
+
+
+def match_comment(string, substring, lineno):
+    if substring == "/":
+        if string[1] == "*":
+            prev = ""
+            for char in string[2:]:
+                substring += char
+                if char == "\n":
+                    lineno += 1
+                if prev == "*" and char == "/":
+                    break
+            prev = char
+
+        if string[1] == "/":
+            for char in string[2:]:
+                substring += char
+                if char == "\n":
+                    lineno += 1
+                    break
+
+        string = strip_from_start(string, substring)
+        return True, string, substring, lineno
+
+    return False, None, None, None
+
+def match_keyword(string, substring):
+    if substring in [s[0] for s in KEYWORD]:
+        for char in string[1:]:
+            substring += char
+            if substring in KEYWORD:
+                string = strip_from_start(string, substring)
+                return True, string, substring
+            if len(substring) <= len(max(KEYWORD, key=len)):
+                break
+
+    return False, None, None
+
+
+def match_id(string, substring):
+    if substring.isalpha():
+        for char in string[1:]:
+            if char.isdigit() or char.isalpha():
+                substring += char
+            else:
+                break
+
+        return True, string, substring
+
+    return False, None, None
+
+
+def match(string, lineno):
+    substring = string[0]
+
+    matched, new_string, matched_string, new_lineno = match_whitespace(string, substring, lineno)
+    if matched:
+        token = "whitespace"
+        return new_string, token, matched_string, new_lineno
+
+    matched, new_string, matched_string = match_num(string, substring)
+    if matched:
+        token = "num"
+        return new_string, token, matched_string, new_lineno
+
+    matched, new_string, matched_string = match_symbol(string, substring)
+    if matched:
+        token = "symbol"
+        return new_string, token, matched_string, lineno
+
+    matched, new_string, matched_string, new_lineno = match_comment(string, substring, lineno)
+    if matched:
+        token = "comment"
+        return new_string, token, matched_string, new_lineno
+
+    matched, new_string, matched_string = match_keyword(string, substring)
+    if matched:
+        token = "keyword"
+        return new_string, token, matched_string, lineno
+
+    matched, new_string, matched_string = match_id(string, substring)
+    if matched:
+        token = "id"
+        return new_string, token, matched_string, lineno
+
+
+def write_output(token, substring, lineno, out):
     if not lineno in out:
         out[lineno] = []
-    out[lineno].append((token, sub_string))
+    out[lineno].append((token, substring))
 
 
 def report_error(discarded, lineno):
     print(f"line {lineno}: {discarded}, Invalid input")
 
 
-def handle_match(matched, lineno, symbol_table, out):
-    token = matched[0]
-    sub_string = matched[1]
+def handle_match(token, substring, lineno, symbol_table, out):
     if token == "id":
-        symbol_table.append(sub_string)
+        symbol_table.append(substring)
 
-    write_output(token, sub_string, lineno, out)
+    write_output(token, substring, lineno, out)
+
+
+def get_next_token(data, symbol_table, lineno, out):
+    data, token, substring, new_lineno = match(data, lineno)
+    handle_match(token, substring, lineno, symbol_table, out)
+    return data, new_lineno
 
 
 def main():
-    with open("test_input.txt") as f:
+    with open("test.txt") as f:
         data = f.read()
         data = data.rstrip("\n")
         f.close
@@ -73,39 +167,10 @@ def main():
     symbol_table = ["if", "else", "void", "int", "while", "break", "continue",
                     "switch", "default", "case", "return"]
     while data:
-        matched = match(data)
-        if matched:
-            handle_match(matched, lineno, symbol_table, out)
-            lineno += matched[1].count("\n")
-            data = data.replace(matched[1], "", 1)
-
-        else:
-            # Remove previous token if it is not whitespace or comment
-            discarded = ""
-            if lineno in out:
-                prev = out[lineno][-1]
-                if prev[0] not in ["whitespace", "comment"]:
-                    if prev[0] == "id":
-                        symbol_table.pop()
-                    tmp = out[lineno].pop()
-                    discarded += tmp[1]
-
-            # Delete input untill next valid token is found
-            while not matched:
-                discarded += data[:1]
-                data = data[1:]
-                matched = match(data)
-
-            handle_match(matched, lineno, symbol_table, out)
-            lineno += matched[1].count("\n")
-            report_error(discarded, lineno)
-            data = data.replace(matched[1], "", 1)
+        data, lineno = get_next_token(data, symbol_table, lineno, out)
 
     print("\nTokens:")
-    for key in out:
-        out[key] = [t for t in out[key] if t[0] not in ["whitespace", "comment"]]
-        if len(out[key]) > 0:
-            print(out[key])
+    print(out)
 
 
     symbol_table = list(dict.fromkeys(symbol_table))
