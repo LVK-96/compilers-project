f5c5bb92149351fa4732d256108b1e2002e3d42f
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-02-20 16:37:52 +0200

    Refactor test.sh

--- parser/test.sh
+++ parser/test.sh
@@ -3,11 +3,13 @@
 for i in {1..6}
 do
     case="T${i}"
+    input="../scanner/${case}/input.txt"
     tokens="../scanner/${case}/tokens.txt"
     symbol_table="../scanner/${case}/symbol_table.txt"
     lexical_errors="../scanner/${case}/lexical_errors.txt"
-    echo "${case}"
-    python parser.py "../scanner/${case}/input.txt"
+
+    echo "${case}:"
+    python parser.py "${input}"
     echo "Tokens: "
     ../are_contents_equal "tokens.txt" "${tokens}"
     echo "Symbol Table: "
