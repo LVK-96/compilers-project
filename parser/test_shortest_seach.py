b265ff1aceab2cc1751d552447078160bac3a42a
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-03-01 18:29:49 +0200

    Shortest construct search

--- parser/test_shortest_seach.py
+++ parser/test_shortest_seach.py
@@ -1,8 +1,3 @@
-import sys
-from collections import deque
-from scanner import Scanner
-from anytree import Node, RenderTree
-
 symbols = ["=", ";", ":", ",", "[", "]", "(", ")", "{", "}",
            "+", "-", "*", "=", "<", "=="]
 keywords = ["if", "else", "void", "int", "while", "break",
@@ -266,242 +261,58 @@ ll1_table = [
         None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
 ]
 
+def find_simplest_construct(popped):
+    options = [popped]
+    shortest = None
+    while not shortest:
+        new_options = options.copy()
+        for o in options:
+            elems = o.split(" ")
+            new_elems = elems.copy()
+            for elem in elems:
+                if elem in non_terminals:
+                    # Expand non terminal
+                    i = non_terminals.index(elem)
+                    row = ll1_table[i]
+                    for rule in row:
+                        if rule and rule != "SYNCH":
+                            # Use rule
+                            i = elems.index(elem)
+                            new_elems[i] = rule
+                            new_option = " ".join(new_elems)
+                            i = options.index(o)
+                            if new_option not in new_options:
+                                if new_options[i] == options[i]:
+                                    new_options[i] = new_option
+                                else:
+                                    new_options.append(new_option)
+                    # Only expand one rule per pass
+                    break 
 
-class LL1_parser:
-    def __init__(self, table, terminals, non_terminals, data, scanner):
-        self.table = table
-        self.terminals = terminals
-        self.non_terminals = non_terminals
-        self.stack = deque(["$", "Program"])
-        self.scanner = scanner
-        self.data = data
-        self.input_ptr = self.scanner.get_next_token()
-        self.tree = Node("Program")
-        self.current_node = self.tree
-        self.errors = []
-        self.lineno = 1
-
-    def remove_node(self, name):
-        # Used in error handling to remove all nonterminals of a node
-        orig_node = self.current_node
-        while True:
-            self.current_node = self.current_node.parent
-            found = False
-            for child in self.current_node.children:
-                if not child.children and child.name == name:
-                    child.parent = None
-                    found = True
+        options = new_options
+        candidates = []
+        for o in options:
+            elems = o.split(" ")
+            # Don't consider epsilons in length
+            elems_no_eps = list(filter(("EPSILON").__ne__, elems))
+            candidate = True
+            for e in elems_no_eps:
+                if e in non_terminals:
+                    candidate = False
                     break
-            if found:
-                break
+            if candidate and len(elems_no_eps) > 0:
+                candidates.append(elems_no_eps)
+        
+        if len(candidates) > 0:
+            shortest = candidates[0]
+            shortest_len = len(candidates[0])
+            for c in candidates:
+                if len(c) < len(shortest):
+                    shortest = c
 
-        # Reset current node to where it was before
-        self.current_node = orig_node
-
-    def report_error(self, lineno, msg):
-        error_msg = f"#{lineno} : Syntax Error, {msg}"
-        self.errors.append(error_msg)
-
-    def find_simplest_construct(self, popped):
-        options = [popped]
-        shortest = None
-        while not shortest:
-            new_options = options.copy()
-            for o in options:
-                elems = o.split(" ")
-                new_elems = elems.copy()
-                for elem in elems:
-                    if elem in non_terminals:
-                        # Expand non terminal
-                        i = non_terminals.index(elem)
-                        row = ll1_table[i]
-                        for rule in row:
-                            if rule and rule != "SYNCH":
-                                # Use rule
-                                i = elems.index(elem)
-                                new_elems[i] = rule
-                                new_option = " ".join(new_elems)
-                                i = options.index(o)
-                                if new_option not in new_options:
-                                    if new_options[i] == options[i]:
-                                        new_options[i] = new_option
-                                    else:
-                                        new_options.append(new_option)
-                        # Only expand one rule per pass
-                        break
-
-            options = new_options
-            candidates = []
-            for o in options:
-                elems = o.split(" ")
-                # Don't consider epsilons in length
-                elems_no_eps = list(filter(("EPSILON").__ne__, elems))
-                candidate = True
-                for e in elems_no_eps:
-                    if e in non_terminals:
-                        candidate = False
-                        break
-                if candidate and len(elems_no_eps) > 0:
-                    candidates.append(elems_no_eps)
-
-            if len(candidates) > 0:
-                shortest = candidates[0]
-                shortest_len = len(candidates[0])
-                for c in candidates:
-                    if len(c) < len(shortest):
-                        shortest = c
-
-        return shortest[0]
-
-    def handle_error(self, production, to_compare):
-        orig_lineno = self.lineno
-        if not production and self.stack[-1] in non_terminals:
-            # Empty production
-            orig_input_ptr = self.input_ptr
-            tmp = self.scanner.get_next_token()
-            self.input_ptr = (tmp[0], tmp[1])
-            self.lineno = tmp[2]
-            if self.input_ptr[1] == "$" and orig_input_ptr[1] == "$":
-                # We are at EOF, empty file and stop parsing
-                for i in range(1, len(self.stack)):
-                    item = self.stack[i]
-                    self.remove_node(item)
-                self.stack = deque([])  # Empty stack so parsing stops
-                self.report_error(orig_lineno + 1, f"Unexpected EndOfFile")
-            else:
-                self.report_error(
-                    orig_lineno, f"illegal {orig_input_ptr[to_compare]}")
-        else:
-            popped = self.stack.pop()
-            if popped in non_terminals:
-                # Synch
-                self.remove_node(popped)
-                self.report_error(
-                    orig_lineno, f'missing "{self.find_simplest_construct(popped)}"')
-            else:
-                # Mismatched terminal at head of stack and input
-                self.report_error(orig_lineno, f'missing "{popped}"')
-
-    def step(self):
-        head = self.stack[-1]
-        to_compare = 1
-        if self.input_ptr[0] == "NUM" or self.input_ptr[0] == "ID":
-            to_compare = 0
-
-        # print("stack", self.stack)
-        # print("input ptr", self.input_ptr)
-
-        if self.input_ptr[to_compare] == head:
-            popped = self.stack.pop()
-            # print("pop", popped)
-
-            if(popped != '$'):
-                self.add_nodes(popped, [], self.input_ptr)
-            tmp = self.scanner.get_next_token()
-            self.input_ptr = (tmp[0], tmp[1])
-            self.lineno = tmp[2]
-
-        else:
-            column = self.terminals.index(self.input_ptr[to_compare])
-            row = self.non_terminals.index(
-                head) if head in self.non_terminals else None
-            production = ll1_table[row][column] if row is not None else None
-            if production and production != "SYNCH":
-                # print("production", production)
-                start = self.stack.pop()
-                items = production.split(" ")
-                for item in items[::-1]:
-                    if (item != "EPSILON"):
-                        self.stack.append(item)
-                # add nodes to tree
-                self.add_nodes(start, items, self.input_ptr)
-            else:
-                # print("error", production)
-                self.handle_error(production, to_compare)
-
-        # print()
-
-    # start is the lhs o production and items make up the rhs
-    def add_nodes(self, start, items, input_ptr):
-        # find the start point
-        while(self.current_node.name != start):
-            # move up
-            self.current_node = self.current_node.parent
-            # check children for start
-            for child in self.current_node.children:
-                if child.name == start and not child.children:
-                    # start found move there
-                    self.current_node = child
-                    break
-
-        left_node = None
-
-        for item in items:
-            left_node = Node(item, parent=self.current_node)
-
-        if self.current_node.name in ["ID", "NUM"]:
-            self.current_node.name = f"({input_ptr[0]}, {input_ptr[1]})"
-
-        if self.current_node.name in symbols:
-            self.current_node.name = f"(SYMBOL, {self.current_node.name})"
-
-        if self.current_node.name in keywords:
-            self.current_node.name = f"(KEYWORD, {self.current_node.name})"
-
-        # move to leftmost node
-        if(left_node):
-            self.current_node = left_node
-
-    def write_tree_to_file(self):
-        with open("parse_tree.txt", "w") as f:
-            for pre, _, node in RenderTree(self.tree.root):
-                print("%s%s" % (pre, node.name), file=f)
-
-    def print_tree(self):
-        for pre, _, node in RenderTree(self.tree.root):
-            print("%s%s" % (pre, node.name))
-
-    def write_errors_to_file(self):
-        with open("syntax_errors.txt", "w") as f:
-            if len(self.errors) > 0:
-                for error in self.errors:
-                    f.write(f"{error}\n")
-            else:
-                f.write("There is no syntax error.")
-
-            f.close()
-
-
-def main(argv):
-    filename = "input.txt"
-    # TODO: Remove this before turn in
-    if argv and argv[0]:
-        filename = argv[0]
-
-    with open(filename) as f:
-        data = f.read()
-        data = data.rstrip("\n")
-        f.close()
-
-    scanner = Scanner(filename, symbols, keywords, data)
-    parser = LL1_parser(
-        ll1_table,
-        terminals,
-        non_terminals,
-        data,
-        scanner
-    )
-    while True:
-        parser.step()
-        if (len(parser.stack) < 1):
-            break
-    parser.write_tree_to_file()
-    parser.write_errors_to_file()
-    scanner.write_tokens_to_file()
-    scanner.write_symbol_table_to_file()
-    scanner.write_errors_to_file()
+    return shortest[0]
 
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    main(argv)
+    for n in non_terminals:
+        find_simplest_construct(n)
