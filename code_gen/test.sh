3429c94653033efe6faf3e2f2d7b2de34ae8d6f2
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-04-04 13:15:05 +0300

    Test script 2.0
    This time it works

---
+++ code_gen/test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+tester="samples/Tester"
+runtime="./tester_Linux.out"
+runtime_output="runtime_output.txt"
+sample_runtime_output="sample_runtime_output.txt"
+
+for i in {1..8}
+do
+    case="T$i"
+    input="samples/${case}/input.txt"
+    sample_compiler_output="samples/${case}/output.txt"
+
+    echo "${case}:"
+    # Run our compiler and pipe the runtime output to a .txt file
+    python parser.py $input
+    cp output.txt $tester
+    cd $tester && pwd && $runtime > $runtime_output 2> /dev/null
+    cd ../..
+
+    echo ""
+
+    # Run our compiler and pipe the runtime output to a .txt file
+    cp $sample_compiler_output $tester
+    cd $tester && $runtime > $sample_runtime_output 2> /dev/null
+    diff -i -w -s -c $runtime_output $sample_runtime_output
+    cd ../..
+done
