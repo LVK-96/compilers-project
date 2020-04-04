2b39f2f88120dc2a99dad953103dd68a86c969d5
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-04-04 12:27:05 +0300

    Revert "Helper scripts for running programs automatically"
    This reverts commit 05ab84a64cb81e170d9ff2510cbd81d97d304de6

--- code_gen/run_sample.sh
+++
@@ -1,7 +0,0 @@
-tester="./samples/Tester"
-sample_runtime_output="${tester}/sample_runtime_out.txt"
-runtime="${tester}/tester_Linux.out"
-compiled_program_path="${tester}/output.txt"
-
-cp $1 $compiled_program_path &&
-$runtime
